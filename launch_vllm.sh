046e6985100751f6a6f0120756913290ec924b91
Cristian Rodriguez <cristian.rodriguez.ud@gmail.com>
2025-03-19 13:18:42 +0000

    Adding vllm launch code corrected

--- launch_vllm.sh
+++ launch_vllm.sh
@@ -1,4 +1,7 @@
-python -m vllm.entrypoints.api_server \
+python -m vllm.entrypoints.openai.api_server \
     --model /content/qwen \
     --tensor-parallel-size 1 \
     --dtype half \
+    --port 8000 \
+    --host 0.0.0.0 \
+    --served-model-name "qwen25"
